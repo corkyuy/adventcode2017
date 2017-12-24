63f5b8b68e8775e8e71007cee9cb42179a053ada
Corky Uy <corky.uy@gmail.com>
2017-12-24 00:12:33 -0500

    Updates: Day2 Part 2

--- day2/Main.hs
+++ day2/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Semigroup ((<>))
+import Data.Ratio
 import Options.Applicative
 import System.IO
 
@@ -31,13 +32,31 @@ rowDiff xs = max - min
     min = minimum xs
     max = maximum xs
 
+rowDivisible :: [Integer] -> Integer
+rowDivisible xs = numerator . divs . head $ filter isEvenlyDivs perm
+  where
+    perm               = [(x, y) | x <- xs
+                                , y <- xs
+                                , x < y]
+    isWholeNum :: Float -> Bool
+    isWholeNum x       = x == fromInteger (round x)
+
+    isEvenlyDivs :: (Integer, Integer) -> Bool
+    isEvenlyDivs (x,y) = isWholeNum $ (fromInteger y) / (fromInteger x)
+    divs  :: (Integer, Integer) -> Rational
+    divs         (x,y) = (toRational y) / (toRational x)
 
 solveDay2 :: Day2Option -> IO ()
 solveDay2 (Day2Option f) = do
   handle <- openFile f ReadMode
   contents <- hGetContents handle
   let rows = fmap words $ lines $ contents
-  let numRows = (fmap . fmap) read rows :: [[Int]]
+  let numRows = (fmap . fmap) read rows :: [[Integer]]
   let diffPerRows = fmap rowDiff numRows
+  let diffPerRows2 = fmap rowDivisible numRows
   putStrLn . show $ sum diffPerRows
+  putStrLn . show $ sum diffPerRows2
   hClose handle
+
+
+
